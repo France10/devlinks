fd8b467bbcccf9bcfb16fd111cd2b340f25944d2
France10 <victor.campos8@gmail.com>
2023-02-09 21:08:19 -0300

    adjust js

--- script.js
+++ script.js
@@ -1,17 +1,16 @@
 function toggleMode() {
   const html = document.documentElement
-  html.classList.toggle('light')
+  html.classList.toggle("light")
   // if(html.classList.contains("light")){
-    //  html.classList.remove('light')
-    // }else{
-      //  html.classList.add('light')
-      //}
-     const img = document.querySelector("#profile img")
-      if(html.classList.contains('light')){
-        img.setAttribute("src","./assets/avatar-light.png")
-      }else{
-        img.setAttribute("src", "./assets/avatar.png")
-      }
-  
-    }
+  //  html.classList.remove('light')
+  // }else{
+  //  html.classList.add('light')
+  //}
+  const img = document.querySelector("#profile img")
+  if (html.classList.contains("light")) {
+    img.setAttribute("src", "./assets/avatar-light.png")
+  } else {
+    img.setAttribute("src", "./assets/avatar.png")
+  }
+}
 
